d53ba21c79be378ba0355e950714592d5fba0216
Ser0jka123 <sersur42@yandex.ru>
2025-12-01 18:00:23 +0700

    upd

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import About from "../../components/Home/About/About";
 import New from "../../components/Home/New/New";  
 import Exclusive from "../../components/Home/Exclusive/Exclusive";  
 import Reviews from "../../components/Home/Reviews/Reviews";
+import PromotionModal from "../../components/PromotionModal/PromotionModal";
 
 export const metadata = {
   title: "IQOS Iluma и стики Terea — купить в Москве с доставкой",
@@ -40,6 +41,7 @@ export default function Home() {
       <New/>
       <About />
       <Reviews />
+      <PromotionModal allowClose={true}/>
     </>
   );
 }
